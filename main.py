d8d03d4e228433676ab452597b9914b0d2046d6e
Yakup Atici <yakup7312@hotmail.com>
2023-05-09 13:15:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import cv2
+
+image = cv2.imread('/Users/jacop/pythonProject/Lab7/WorldCupMessi.jpeg')
+
+blue, green, red = cv2.split(image)
+
+cv2.imshow('Blue Channel', blue)
+cv2.imshow('Green Channel', green)
+cv2.imshow('Red Channel', red)
+
+green *=0
+
+new_image = cv2.merge((blue, green , red))
+
+cv2.imshow('New Image', new_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
